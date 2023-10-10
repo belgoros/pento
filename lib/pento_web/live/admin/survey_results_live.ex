b7e9e13e96e2075b165d5d44906317fcec4dd468
belgoros <s.cambour@gmail.com>
2023-10-10 12:00:56 +0200

    Implement ratings filtering by age

--- lib/pento_web/live/admin/survey_results_live.ex
+++ lib/pento_web/live/admin/survey_results_live.ex
@@ -9,10 +9,35 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
     {:ok,
      socket
      |> assign(assigns)
+     |> assign_age_group_filter()
      |> assign_products_with_average_ratings()
      |> assign_dataset()
      |> assign_chart()
      |> assign_chart_svg()}
+  end
+
+  @impl true
+  def handle_event(
+        "age_group_filter",
+        %{"age_group_filter" => age_group_filter},
+        socket
+      ) do
+    {:noreply,
+     socket
+     |> assign_age_group_filter(age_group_filter)
+     |> assign_products_with_average_ratings()
+     |> assign_dataset()
+     |> assign_chart()
+     |> assign_chart_svg()}
+  end
+
+  def assign_age_group_filter(socket) do
+    socket
+    |> assign(:age_group_filter, "all")
+  end
+
+  def assign_age_group_filter(socket, age_group_filter) do
+    assign(socket, :age_group_filter, age_group_filter)
   end
 
   def assign_dataset(
@@ -70,11 +95,13 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
     "stars"
   end
 
-  defp assign_products_with_average_ratings(socket) do
+  defp assign_products_with_average_ratings(
+         %{assigns: %{age_group_filter: age_group_filter}} = socket
+       ) do
     socket
     |> assign(
       :products_with_average_ratings,
-      Catalog.products_with_average_ratings()
+      Catalog.products_with_average_ratings(%{age_group_filter: age_group_filter})
     )
   end
 end
