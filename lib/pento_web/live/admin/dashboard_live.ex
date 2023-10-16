76668f71bd0ded4973653e911c9758222c0ddec5
belgoros <s.cambour@gmail.com>
2023-10-16 17:37:19 +0200

    Implement product rating broadcasting

--- lib/pento_web/live/admin/dashboard_live.ex
+++ lib/pento_web/live/admin/dashboard_live.ex
@@ -1,10 +1,29 @@
 defmodule PentoWeb.Admin.DashboardLive do
   use PentoWeb, :live_view
 
+  alias PentoWeb.Admin.{SurveyResultsLive}
+  alias PentoWeb.Endpoint
+
+  @survey_results_topic "survey_results"
+
   @impl true
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Endpoint.subscribe(@survey_results_topic)
+    end
+
     {:ok,
      socket
      |> assign(:survey_results_component_id, "survey-results")}
+  end
+
+  @impl true
+  def handle_info(%{event: "rating_created"}, socket) do
+    send_update(
+      SurveyResultsLive,
+      id: socket.assigns.survey_results_component_id
+    )
+
+    {:noreply, socket}
   end
 end
