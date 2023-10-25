1ea612b8c7db1baa37500b45cc5cc6b1c8a584fb
belgoros <s.cambour@gmail.com>
2023-10-25 16:16:00 +0200

    Finish Presence implementation

--- lib/pento_web/live/admin/dashboard_live.ex
+++ lib/pento_web/live/admin/dashboard_live.ex
@@ -1,20 +1,23 @@
 defmodule PentoWeb.Admin.DashboardLive do
   use PentoWeb, :live_view
 
-  alias PentoWeb.Admin.{SurveyResultsLive}
+  alias PentoWeb.Admin.{SurveyResultsLive, UserActivityLive}
   alias PentoWeb.Endpoint
 
   @survey_results_topic "survey_results"
+  @user_activity_topic "user_activity"
 
   @impl true
   def mount(_params, _session, socket) do
     if connected?(socket) do
       Endpoint.subscribe(@survey_results_topic)
+      Endpoint.subscribe(@user_activity_topic)
     end
 
     {:ok,
      socket
-     |> assign(:survey_results_component_id, "survey-results")}
+     |> assign(:survey_results_component_id, "survey-results")
+     |> assign(:user_activity_component_id, "user-activity")}
   end
 
   @impl true
@@ -22,6 +25,16 @@ defmodule PentoWeb.Admin.DashboardLive do
     send_update(
       SurveyResultsLive,
       id: socket.assigns.survey_results_component_id
+    )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{event: "presence_diff"}, socket) do
+    send_update(
+      UserActivityLive,
+      id: socket.assigns.user_activity_component_id
     )
 
     {:noreply, socket}
